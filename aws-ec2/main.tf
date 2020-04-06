50ff56704dbd076d2fa1caa933d795b6d570682b
John Tucker <john@larkintuckerllc.com>
2020-04-05 20:08:20 -0400

    security group

--- aws-ec2/main.tf
+++ aws-ec2/main.tf
@@ -1,3 +1,35 @@
 provider "aws" {
   region = "us-east-1"
 }
+
+resource "aws_security_group" "this" {
+  name   = "aws-ec2"
+  vpc_id = var.vpc_id
+}
+
+resource "aws_security_group_rule" "this_ingress_https" {
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 443
+  protocol    = "tcp"
+  security_group_id = aws_security_group.this.id
+  to_port     = 443
+  type        = "ingress"
+}
+
+resource "aws_security_group_rule" "this_ingress_ssh" {
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 22
+  protocol    = "tcp"
+  security_group_id = aws_security_group.this.id
+  to_port     = 22
+  type        = "ingress"
+}
+
+resource "aws_security_group_rule" "this_egress" {
+  cidr_blocks = ["0.0.0.0/0"]
+  from_port   = 0
+  protocol    = "-1"
+  security_group_id = aws_security_group.this.id
+  to_port     = 0
+  type        = "egress"
+}
