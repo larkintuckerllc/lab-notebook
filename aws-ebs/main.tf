e27f32da9b6b220ad877c4a0e0a1c491c3f34229
John Tucker <john@larkintuckerllc.com>
2020-04-11 05:15:17 -0400

    cleanup

--- aws-ebs/main.tf
+++ aws-ebs/main.tf
@@ -31,43 +31,43 @@ data "aws_ami" "this" {
   }
 }
 
-resource "aws_security_group" "ec2" {
-  name   = "${local.identifier}-ec2"
+resource "aws_security_group" "this" {
+  name   = local.identifier
   vpc_id = var.vpc_id
 }
 
-resource "aws_security_group_rule" "ec2_ingress_all" {
+resource "aws_security_group_rule" "this_ingress_all" {
   from_port   = 0
   protocol    = "-1"
-  security_group_id = aws_security_group.ec2.id
-  source_security_group_id = aws_security_group.ec2.id
+  security_group_id = aws_security_group.this.id
+  source_security_group_id = aws_security_group.this.id
   to_port     = 0
   type        = "ingress"
 }
 
-resource "aws_security_group_rule" "ec2_ingress_ssh" {
+resource "aws_security_group_rule" "this_ingress_ssh" {
   cidr_blocks = ["0.0.0.0/0"]
   from_port   = 22
   protocol    = "tcp"
-  security_group_id = aws_security_group.ec2.id
+  security_group_id = aws_security_group.this.id
   to_port     = 22
   type        = "ingress"
 }
 
-resource "aws_security_group_rule" "ec2_ingress_html" {
+resource "aws_security_group_rule" "this_ingress_html" {
   cidr_blocks = ["0.0.0.0/0"]
   from_port   = 80
   protocol    = "tcp"
-  security_group_id = aws_security_group.ec2.id
+  security_group_id = aws_security_group.this.id
   to_port     = 80
   type        = "ingress"
 }
 
-resource "aws_security_group_rule" "ec2_egress" {
+resource "aws_security_group_rule" "this_egress" {
   cidr_blocks = ["0.0.0.0/0"]
   from_port   = 0
   protocol    = "-1"
-  security_group_id = aws_security_group.ec2.id
+  security_group_id = aws_security_group.this.id
   to_port     = 0
   type        = "egress"
 }
@@ -86,7 +86,7 @@ resource "aws_instance" "this" {
   }
   instance_type          = "t2.micro"
   key_name               = var.key_name
-  vpc_security_group_ids = [aws_security_group.ec2.id]
+  vpc_security_group_ids = [aws_security_group.this.id]
   subnet_id              = data.aws_subnet.this.id
   tags = {
     Name = local.identifier
