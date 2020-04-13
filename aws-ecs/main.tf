d9f2a7ab87a733e477ddfb4c609d19bb66cca3cc
John Tucker <john@larkintuckerllc.com>
2020-04-13 18:09:13 -0400

    security

--- aws-ecs/main.tf
+++ aws-ecs/main.tf
@@ -29,3 +29,71 @@ resource "aws_dynamodb_table" "this" {
   write_capacity   = 1
 }
 
+data "aws_ecr_repository" "this" {
+  name = local.identifier
+}
+
+resource "aws_iam_role" "execution" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  name               = "${local.identifier}-execution"
+}
+
+resource "aws_iam_role_policy_attachment" "execution" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
+  role       = aws_iam_role.execution.name
+}
+
+resource "aws_iam_role" "task" {
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Sid": "",
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ecs-tasks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+  name               = "${local.identifier}-task"
+}
+
+resource "aws_iam_role_policy" "task" {
+    name   = "DynamoDBWriteTodos"
+    policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "VisualEditor0",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:PutItem",
+                "dynamodb:DeleteItem",
+                "dynamodb:Scan"
+            ],
+            "Resource": "arn:aws:dynamodb:us-east-1:143287522423:table/Todos"
+        }
+    ]
+}
+EOF
+    role   = aws_iam_role.task.id
+}
