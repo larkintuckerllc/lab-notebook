07246fc474169403b8119fa75ec19f80f2f8a23f
John Tucker <john@larkintuckerllc.com>
2020-04-22 12:39:13 -0400

    done

--- k8s-template-terraform/infrastructure/main.tf
+++ k8s-template-terraform/infrastructure/main.tf
@@ -2,18 +2,53 @@ provider "kubernetes" {
 }
 
 resource "kubernetes_service" "this" {
-  labels = {
-    release = "a"
-  }
   metadata {
-    name = "api"
+    labels = {
+      release = var.release
+    }
+    name = var.app
   }
   spec {
-    selector = {
-      app = "api"
-    }
     port {
       port = 80
+    }
+    selector = {
+      app = var.app
+    }
+  }
+}
+
+resource "kubernetes_deployment" "this" {
+  depends_on = [kubernetes_service.this]
+  metadata {
+    labels = {
+      release = var.release
+    }
+    name = var.app
+  }
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        app = var.app
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          app     = var.app
+          release = var.release
+        }
+      }
+      spec {
+        container {
+          image = "localhost:32000/api"
+          name = "app"
+          port {
+            container_port = 80
+          }
+        }
+      }
     }
   }
 }
